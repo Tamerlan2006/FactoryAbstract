0bd1f9dd9597ac406226b1862987ae52afb65db2
Tamerlan2006 <241648@astanait.edu.kz>
2025-09-20 18:58:03 +0500

    fixed

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String type = sc.nextLine().trim().toLowerCase();
+        try {
+            Application app = ApplicationFactory.createApplication(type);
+            app.display();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        sc.close();
+    }
 }
